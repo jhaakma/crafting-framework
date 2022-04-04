8ec1b61831d6a2d3e8fccbf907b36be8a7cd750b
jhaakma <jchaakma@gmail.com>
2022-04-05 04:20:30 +1200

    Allow recovery of materials from equipment

--- Data Files/MWSE/mods/CraftingFramework/main.lua
+++ Data Files/MWSE/mods/CraftingFramework/main.lua
@@ -2,6 +2,7 @@ require("CraftingFramework.mcm")
 local function onInit()
     require("CraftingFramework.controllers.StaticActivator")
     require("CraftingFramework.controllers.Positioner")
+    require("CraftingFramework.controllers.RecoverMaterials")
 
     mwse.log("[CraftingFramework INFO] StaticActivator Controller initialised.")
 end
