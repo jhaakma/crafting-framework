9b244cb2d80c4d4aeb1a7b3e57282d79fa3a9d2e
jhaakma <jchaakma@gmail.com>
2022-05-12 17:28:06 +1200

    Fix bug opening crafted containers

--- Data Files/MWSE/mods/CraftingFramework/controllers/CraftingEvents.lua
+++ Data Files/MWSE/mods/CraftingFramework/controllers/CraftingEvents.lua
@@ -1,10 +1,12 @@
 local Util = require("CraftingFramework.util.Util")
 local Craftable = require("CraftingFramework.components.Craftable")
 local logger = Util.createLogger("CraftingEvents")
+
 local function craftableActivated(e)
-    logger:debug("craftableActivated!!")
-    local craftable = Craftable.getPlacedCraftable(e.reference.object.id:lower())
-    if craftable and craftable:getPlacedObjectId() then
+    logger:trace("craftableActivated object id: %s", e.reference.baseObject.id)
+    local craftable = Craftable.getPlacedCraftable(e.reference.baseObject.id:lower())
+    if craftable then
+        logger:trace("craftableActivated placedObject id: %s", craftable:getPlacedObjectId())
         if Util.isShiftDown() and Util.canBeActivated(e.reference) then
             e.reference.data.allowActivate = true
             tes3.player:activate(e.reference)
@@ -17,14 +19,14 @@ end
 event.register("CraftingFramework:CraftableActivated", craftableActivated)
 
 
+---@param e itemDroppedEventData
 local function itemDropped(e)
-    logger:debug("ItemDROPPPPPED")
-    local craftable = Craftable.getCraftable(e.reference.object.id)
+    local craftable = Craftable.getCraftable(e.reference.baseObject.id)
     local placedObject = craftable and craftable:getPlacedObjectId()
     if placedObject then
-        logger:debug("placedObject: " .. placedObject)
+        logger:trace("placedObject: " .. placedObject)
         if placedObject and e.reference.baseObject.id:lower() == craftable.id then
-            logger:debug("itemDropped: " .. placedObject)
+            logger:trace("itemDropped placedObject: " .. placedObject)
             craftable:swap(e.reference)
         end
     end
